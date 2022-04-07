d7222f50b501ee369b3f04da2e31f417932f4733
bbejczy <bbejczy@icloud.com>
2022-04-07 11:18:23 +0900

    requiements file

--- DDQN_ER_open.py
+++ DDQN_ER_open.py
@@ -1,8 +1,7 @@
-from turtle import forward
-import gym
 import collections
 import random
 
+import gym
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,16 +12,17 @@ learning_rate = 0.0005
 gamma = 0.98
 max_episode = 20000
 buffer_limit = 10000
-batch_size = 32 #TODO: define batch size
-initial_exp = 2000 #TODO: define initial experience
+batch_size = 32  # TODO: define batch size
+initial_exp = 2000  # TODO: define initial experience
 
-class ReplayBuffer():
+
+class ReplayBuffer:
     def __init__(self) -> None:
         self.buffer = collections.deque(maxlen=buffer_limit)
 
     def put(self, transition):
         self.buffer.append(transition)
-    
+
     def sample(self, n):
         mini_batch = random.sample(self.buffer, n)
         s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
@@ -35,33 +35,41 @@ class ReplayBuffer():
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
 
-        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), torch.tensor(done_mask_lst)
+        return (
+            torch.tensor(s_lst, dtype=torch.float),
+            torch.tensor(a_lst),
+            torch.tensor(r_lst),
+            torch.tensor(s_prime_lst, dtype=torch.float),
+            torch.tensor(done_mask_lst),
+        )
 
     def size(self):
         return len(self.buffer)
+
 
 class Qnet(nn.Module):
     def __init__(self):
         super(Qnet, self).__init__()
         self.fc1 = nn.Linear(4, 128)
-                                # TODO: design network architecture
+        # TODO: design network architecture
         self.fc2 = nn.Linear(128, 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-                                # TODO: design forward pass
+        # TODO: design forward pass
         x = self.fc2(x)
         return x
 
     def sample_action(self, obs, epsilon):
-        
+
         q_out = self.forward(obs)
         r_value = random.random()
 
         if r_value < epsilon:
-            return random.randint(0,1)
+            return random.randint(0, 1)
         else:
             return q_out.argmax().item()
+
 
 def train(q, q_target, memory, optimizer):
 
@@ -69,7 +77,7 @@ def train(q, q_target, memory, optimizer):
 
         s, a, r, s_prime, done_mask = memory.sample(batch_size)
         q_out = q(s)
-        q_a = q_out.gather(1,a)
+        q_a = q_out.gather(1, a)
         max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
         target = r + gamma * max_q_prime * done_mask
 
@@ -78,9 +86,10 @@ def train(q, q_target, memory, optimizer):
         loss.backward()
         optimizer.step()
 
+
 def main():
 
-    env = gym.make('CartPole-v1')
+    env = gym.make("CartPole-v1")
 
     q = Qnet()
     q_tagret = Qnet()
@@ -89,11 +98,11 @@ def main():
     memory = ReplayBuffer()
     print_interval = 20
     score = 0.0
-    optimizer = optim.Adam(q.parameters(), lr = learning_rate)
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
-    for n_epi in range (max_episode):
+    for n_epi in range(max_episode):
 
-        epsilon = max(0.01, 0.1 - 0.01*(n_epi/200))
+        epsilon = max(0.01, 0.1 - 0.01 * (n_epi / 200))
         s = env.reset()
         done = False
 
@@ -104,25 +113,26 @@ def main():
             s_prime, r, done, info = env.step(a)
 
             done_mask = 0.0 if done else 1.0
-            memory.put((s, a, r/100.0, s_prime, done_mask))
-            s= s_prime
+            memory.put((s, a, r / 100.0, s_prime, done_mask))
+            s = s_prime
 
             score += r
 
         if memory.size() > initial_exp:
-            train(q,q_tagret, memory, optimizer)
-        
+            train(q, q_tagret, memory, optimizer)
+
         if n_epi % print_interval == 0 and n_epi != 0:
             q_tagret.load_state_dict(q.state_dict())
-            print("n_episode : {}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(n_epi, score/print_interval, memory.size(), epsilon*100))
+            print(
+                "n_episode : {}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                    n_epi, score / print_interval, memory.size(), epsilon * 100
+                )
+            )
 
             score = 0.0
-    
+
     env.close()
 
 
-
-
-
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
