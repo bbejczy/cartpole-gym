d02311e66db6fe0f8ac45c1d70140fdef39a0908
bbejczy <bbejczy@icloud.com>
2022-04-11 22:29:10 +0900

    parameter parsing, early network + wandb

--- DDQN_ER_open.py
+++ DDQN_ER_open.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import random
 
@@ -7,18 +8,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-# Hyperparameters
+import wandb
+
+"""
+Provided Hyperparameters
+
 learning_rate = 0.0005
 gamma = 0.98
 max_episode = 20000
 buffer_limit = 10000
 batch_size = 32  # TODO: define batch size
-initial_exp = 2000  # TODO: define initial experience
+initial_exp = 5000  # TODO: define initial experience
+
+"""
 
 
 class ReplayBuffer:
-    def __init__(self) -> None:
-        self.buffer = collections.deque(maxlen=buffer_limit)
+    def __init__(self, args) -> None:
+        self.buffer = collections.deque(maxlen=args.buffer_limit)
 
     def put(self, transition):
         self.buffer.append(transition)
@@ -50,14 +57,24 @@ class ReplayBuffer:
 class Qnet(nn.Module):
     def __init__(self):
         super(Qnet, self).__init__()
-        self.fc1 = nn.Linear(4, 128)
-        # TODO: design network architecture
-        self.fc2 = nn.Linear(128, 2)
+
+        self.input_dim = 4
+        self.output_dim = 2
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+        )
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        # TODO: design forward pass
-        x = self.fc2(x)
+        x = self.model(x)
         return x
 
     def sample_action(self, obs, epsilon):
@@ -71,15 +88,15 @@ class Qnet(nn.Module):
             return q_out.argmax().item()
 
 
-def train(q, q_target, memory, optimizer):
+def train(q, q_target, memory, optimizer, args):
 
     for i in range(10):
 
-        s, a, r, s_prime, done_mask = memory.sample(batch_size)
+        s, a, r, s_prime, done_mask = memory.sample(args.batch_size)
         q_out = q(s)
         q_a = q_out.gather(1, a)
         max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
-        target = r + gamma * max_q_prime * done_mask
+        target = r + args.gamma * max_q_prime * done_mask
 
         loss = F.smooth_l1_loss(q_a, target)
         optimizer.zero_grad()
@@ -87,10 +104,15 @@ def train(q, q_target, memory, optimizer):
         optimizer.step()
 
 
-def main():
+def main(args):
 
     global device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    device = "cpu" if args.gpu == -1 else f"cuda:{args.gpu}"
+
+    if args.wandb:
+        wandb.init(project="cartpole-gym", monitor_gym=args.monitor_gym)
+        wandb.config.update(args)
 
     env = gym.make("CartPole-v1")
 
@@ -98,12 +120,12 @@ def main():
     q_tagret = Qnet().to(device)
     q_tagret.load_state_dict(q.state_dict())
 
-    memory = ReplayBuffer()
+    memory = ReplayBuffer(args)
     print_interval = 20
     score = 0.0
-    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(q.parameters(), lr=args.learning_rate)
 
-    for n_epi in range(max_episode):
+    for n_epi in range(args.max_episode):
 
         epsilon = max(0.01, 0.1 - 0.01 * (n_epi / 200))
         s = env.reset()
@@ -121,8 +143,8 @@ def main():
 
             score += r
 
-        if memory.size() > initial_exp:
-            train(q, q_tagret, memory, optimizer)
+        if memory.size() > args.initial_exp:
+            train(q, q_tagret, memory, optimizer, args)
 
         if n_epi % print_interval == 0 and n_epi != 0:
             q_tagret.load_state_dict(q.state_dict())
@@ -131,6 +153,13 @@ def main():
                     n_epi, score / print_interval, memory.size(), epsilon * 100
                 )
             )
+            wandb.log(
+                {
+                    "n_episode": n_epi,
+                    "score": score / print_interval,
+                    "eps": epsilon * 100,
+                }
+            )
 
             score = 0.0
 
@@ -138,4 +167,20 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser(description="cartpole-gym")
+    parser.add_argument(
+        "--gpu", type=int, default=0, help="gpu device num. -1 is for cpu"
+    )
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--initial_exp", type=int, default=500)
+    parser.add_argument("--max_episode", type=int, default=20000)
+    parser.add_argument("--buffer_limit", type=int, default=10000)
+    parser.add_argument("--gamma", type=float, default=0.98)
+    parser.add_argument("--learning_rate", type=float, default=5e-4)
+    parser.add_argument("--wandb", type=bool, default=False)
+    parser.add_argument("--monitor_gym", type=bool, default=False)
+
+    args = parser.parse_args()
+
+    main(args)
