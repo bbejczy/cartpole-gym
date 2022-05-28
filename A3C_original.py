5941cb26bfc60d0875b406eda022a36da5ceded4
bbejczy <bbejczy@icloud.com>
2022-05-28 15:44:40 +0900

    A3C continuous

--- A3C_original.py
+++ A3C_original.py
@@ -3,17 +3,15 @@ import torch
 import torch.nn as nn               # Linear
 import torch.nn.functional as F     # relu, softmax
 import torch.optim as optim         # Adam Optimizer
-from torch.distributions import Categorical, Normal # Categorical import from torch.distributions module
+from torch.distributions import Categorical # Categorical import from torch.distributions module
 import torch.multiprocessing as mp # multi processing
 import time 
-import warnings
-warnings.filterwarnings("ignore")
 
 from matplotlib import pyplot as plt ###for plot
 import numpy as np
 
 # Hyperparameters
-n_train_processes = 3      # number of independent agents. (for training)
+n_train_processes = 1      # number of independent agents. (for training)
 learning_rate     = 0.0002
 update_interval   = 5      # Collect data during 'update_interval steps' and proceed with training.  
 gamma = 0.98
@@ -24,35 +22,24 @@ max_test_ep  = 400  # max episode for test.
 class ActorCritic(nn.Module):  # ActorCritic Class - Created by inheriting nn.Module (provided by Pytorch) Class. 
     def __init__(self):        # constructor - initializer(__init__): Object creation and variable initialization.
         super(ActorCritic, self).__init__() # Calling the constructor of the inherited parent Class(nn.Module).
-        self.fc1   = nn.Linear(11, 256)  # Fully Connected: input   4 --> output 256
-        self.fc_pi = nn.Linear(256, 3)  # Fully Connected: input 256 --> output   2
+        self.fc1   = nn.Linear(4, 256)  # Fully Connected: input   4 --> output 256
+        self.fc_pi = nn.Linear(256, 2)  # Fully Connected: input 256 --> output   2
         self.fc_v  = nn.Linear(256, 1)  # Fully Connected: input 256 --> output   1
-        self.std = nn.Linear(256, 3)
 
     def pi(self, x, softmax_dim=0):  # In the case of batch processing, softmax_dim becomes 1. (default 0)
         x = F.relu(self.fc1(x))
-        std = F.relu(self.std(x))
-        std = torch.clamp(std, min = -20, max=2)
-        std = std.exp()
-        # print(std)
         x = self.fc_pi(x)
-        
         # x    = tensor([ [-0.1170, -0.1489], 
         #                 [-0.0939, -0.1290],
         #                  ...,
         #                 [-0.1168, -0.1281] ], grad_fn=<AddmmBackward0>)        
-        # prob = F.softmax(x, dim=softmax_dim) # x[5][2] ==> Perform the softmax in the direction "dim=1". ==> batch mode: batch_size(5) x 2 # (update_interval = 5)
+        prob = F.softmax(x, dim=softmax_dim) # x[5][2] ==> Perform the softmax in the direction "dim=1". ==> batch mode: batch_size(5) x 2 # (update_interval = 5)
                                              # x[2]    ==> Perform the softmax in the direction "dim=0".
-        # prob = F.tanh(x) #NOTE: this dimension could become a problem
         # prob = tensor([ [0.5080, 0.4920], 
         #                 [0.5088, 0.4912],
         #                  ...,
-        #                 [0.5028, 0.4972] ], grad_fn=<SoftmaxBackward0>)
-        # std =  torch.ones(1,3)*1e-2
-        dist = Normal(x, std)
-        # print("~~~~~",std.shape)
-                                                     
-        return dist
+        #                 [0.5028, 0.4972] ], grad_fn=<SoftmaxBackward0>)                                             
+        return prob
 
     def v(self, x):
         x = F.relu(self.fc1(x))
@@ -65,7 +52,7 @@ def train(global_model, rank): # Called by 3(n_train_processes) agents independe
 
     optimizer = optim.Adam(global_model.parameters(), lr=learning_rate) # The optimizer updates the parameters of 'global_model'.
 
-    env = gym.make('Hopper-v3') # Create 'CartPole-v1' environment. 
+    env = gym.make('CartPole-v1') # Create 'CartPole-v1' environment. 
 
     for n_epi in range(max_train_ep):  #max_train_ep = 300
         done = False     # done becomes True when the episode ends.
@@ -73,23 +60,16 @@ def train(global_model, rank): # Called by 3(n_train_processes) agents independe
 
         while not done: # CartPole-v1 forced to terminates at 500 steps.
 
-            s_lst, a_lst, r_lst, p_lst = [], [], [], []
+            s_lst, a_lst, r_lst = [], [], []
 
             for t in range(update_interval):   # Collect data during 5(update_interval) steps and proceed with training.  
                 prob = local_model.pi(torch.from_numpy(s).float())
-                # dist = Normal(prob,local_model.log_std)
-                a = prob.sample()
-                log_prob = prob.log_prob(a).sum()
-                # log_prob = log_prob.detach()
-                a = F.tanh(a)
-                a = a.reshape(-1).numpy()
                 # torch.from_numpy(s).float() = tensor([-0.0411,  0.0121, -0.0213, -0.0273]) : Casting numpy array To Tensor float
                 # prob =  tensor([0.4492, 0.5508], grad_fn=<SoftmaxBackward0>)
-                # m = Categorical(prob) # Categorical probability distribution model
-                # a = prob.sample().item() # tensor(0) or tensor(1)
+                m = Categorical(prob) # Categorical probability distribution model
+                a = m.sample().item() # tensor(0) or tensor(1)
                 # a =  tensor(0) 
                 # a.item() = 0   - .item()-Casting a single tensor to a Python variable
-                # print(a)
                 s_prime, r, done, info = env.step(a)
                 # s_prime = [-0.0408579  -0.18269246 -0.0218931   0.25857786]
                 # r =  1.0
@@ -97,7 +77,6 @@ def train(global_model, rank): # Called by 3(n_train_processes) agents independe
 
                 s_lst.append(s)
                 a_lst.append([a])
-                p_lst.append(log_prob)
                 r_lst.append(r/100.0)
 
                 s = s_prime
@@ -112,24 +91,18 @@ def train(global_model, rank): # Called by 3(n_train_processes) agents independe
                 td_target_lst.append([R])
             td_target_lst.reverse()
 
-            s_batch, a_batch, td_target_batch, p_batch = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), torch.tensor(td_target_lst), torch.stack(p_lst)
+            s_batch, a_batch, td_target_batch = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), torch.tensor(td_target_lst)
             advantage = td_target_batch - local_model.v(s_batch)
 
             pi = local_model.pi(s_batch, softmax_dim=1)
-
-            # prob = local_model.pi(s_batch)
-            # log_prob = prob.log_prob(a).sum()
             # pi = tensor([ [0.5080, 0.4920], 
             #               [0.5088, 0.4912],
             #                ...,
             #               [0.5028, 0.4972] ], grad_fn=<SoftmaxBackward0>)            
-            # pi_a = pi.gather(1, a_batch)
+            pi_a = pi.gather(1, a_batch)
             # a_batch  = [       [0],      [1], ...,      [0] ]
-            # pi_a     = [ [0.05080], [0.4912], ..., [0.5028] ]  
-            # NOTE: -pi_a.log_prob(a_batch)     
-            # loss = -p_batch * advantage.detach() + F.smooth_l1_loss(local_model.v(s_batch), td_target_batch.detach()) # This is equivalent to Actor-Critic.
-            loss = torch.sum(-p_batch * advantage.detach().reshape(-1).float()) + F.mse_loss(local_model.v(s_batch), td_target_batch.detach().float()) # This is equivalent to Actor-Critic.
-            # loss = -p_batch * advantage.detach() + F.mse_loss(local_model.v(s_batch), td_target_batch.detach()) # This is equivalent to Actor-Critic.
+            # pi_a     = [ [0.05080], [0.4912], ..., [0.5028] ]            
+            loss = -torch.log(pi_a) * advantage.detach() + F.smooth_l1_loss(local_model.v(s_batch), td_target_batch.detach()) # This is equivalent to Actor-Critic.
             # " .detach()" means do not update "advantage" and "td_target_batch".
 
             optimizer.zero_grad()  # In PyTorch, since the differential value is accumulated, the gradient must be initialized.
@@ -143,59 +116,6 @@ def train(global_model, rank): # Called by 3(n_train_processes) agents independe
 
     env.close()
     print("Training process {} reached maximum episode.".format(rank))
-
-
-def test(global_model):
-    env = gym.make('Hopper-v3') # Create 'CartPole-v1' environment. 
-    score = 0.0           
-    print_interval = 20   
-
-    render = False # for rendering
-
-    reward_means   = []   ###for plot
-    reward_stds    = []   ###for plot
-    rwd_list       = []   ###for plot 
-
-    for n_epi in range(max_test_ep):  # max_test_ep = 400
-        done = False    # done becomes True when the episode ends.
-        s = env.reset() # Reset Environment
-
-        rwd_sum = 0.0 # Sum of rewards for each episode
-
-        while not done:
-            prob = global_model.pi(torch.from_numpy(s).float())
-            # a = Categorical(prob).sample().item()
-            # a = prob.sample().item()
-            # dist = Normal(prob,global_model.log_std)
-            a = prob.sample()
-            a = F.tanh(a)
-            a = a.reshape(-1).numpy()
-            # print(a)
-            s_prime, r, done, info = env.step(a)
-            s = s_prime
-
-            score   += r  # Sum of rewards for max_test_ep episode
-            rwd_sum += r  # Sum of rewards for each episode
-
-            if render:  # for rendering
-                env.render()
-
-        rwd_list.append(rwd_sum)        
-
-        if n_epi % print_interval == 0 and n_epi != 0:
-            print( "# of episode :{:4d}, avg score : {:7.1f}, std : {:7.1f}".format(n_epi, score/print_interval, np.std(rwd_list)) )
-            reward_means.append(score/print_interval) ###for plot
-            reward_stds.append(np.std(rwd_list))      ###for plot
-            rwd_list = []
-
-            if abs(score/print_interval)>450:   # for rendering
-                render = False             
-            score = 0.0
-            time.sleep(1) #1 second.
-
-    env.close()
-
-    print_reward(reward_means, reward_stds, print_interval, 'A3C', 'g') #label name, color='r','g','b','c','m','y','k','w' 
 
 
 def print_reward(rwds, stds, eval_interval, label_name, color): #for plot
@@ -233,6 +153,53 @@ def print_reward(rwds, stds, eval_interval, label_name, color): #for plot
     ax.spines['left'].set_visible(False)
     
     plt.show()
+
+def test(global_model):
+    env = gym.make('CartPole-v1') # Create 'CartPole-v1' environment. 
+    score = 0.0           
+    print_interval = 20   
+
+    render = False # for rendering
+
+    reward_means   = []   ###for plot
+    reward_stds    = []   ###for plot
+    rwd_list       = []   ###for plot 
+
+    for n_epi in range(max_test_ep):  # max_test_ep = 400
+        done = False    # done becomes True when the episode ends.
+        s = env.reset() # Reset Environment
+
+        rwd_sum = 0.0 # Sum of rewards for each episode
+
+        while not done:
+            prob = global_model.pi(torch.from_numpy(s).float())
+            a = Categorical(prob).sample().item()
+            s_prime, r, done, info = env.step(a)
+            s = s_prime
+
+            score   += r  # Sum of rewards for max_test_ep episode
+            rwd_sum += r  # Sum of rewards for each episode
+
+            if render:  # for rendering
+                env.render()
+
+        rwd_list.append(rwd_sum)        
+
+        if n_epi % print_interval == 0 and n_epi != 0:
+            print( "# of episode :{:4d}, avg score : {:7.1f}, std : {:7.1f}".format(n_epi, score/print_interval, np.std(rwd_list)) )
+            reward_means.append(score/print_interval) ###for plot
+            reward_stds.append(np.std(rwd_list))      ###for plot
+            rwd_list = []
+
+            if abs(score/print_interval)>450:   # for rendering
+                render = False             
+            score = 0.0
+            time.sleep(1) #1 second.
+
+    env.close()
+
+    print_reward(reward_means, reward_stds, print_interval, 'A3C', 'g') #label name, color='r','g','b','c','m','y','k','w' 
+
 
 if __name__ == '__main__':
     global_model = ActorCritic()
